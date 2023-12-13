b70575c4e95416732969dc604d3bba8b35671437
[SohaibMagrabi] <[Sohaibmagrabi@gmail.com]>
2023-12-13 18:58:58 +0300

    handeler strikout &delete

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([""])
+  const [todos, setTodos] = useState([])
   const inputRef = useRef()
+  
   const handleAddToDo = () => {
     const txt = inputRef.current.value;
-    setTodos([...todos, txt]);//خد القديمة وضف عليها الجديدة
-    inputRef.current.value ='';
+    const newItem = { completed: false, txt }
+    setTodos([...todos, newItem]);//خد القديمة وضف عليها الجديدة
+    inputRef.current.value = '';
     console.log(txt)
+  }
+  const handleItemDone = (i) => {
+    const newTodos = [...todos]; //or =todos.slice()
+    newTodos[i].completed = !newTodos[i].completed;//اعكس حالتها
+    setTodos(newTodos);
+    //  console.log(setTodos[i].completed)
+  };
+  const handleDeleteItem = (i) => {
+    const newTodos = [...todos];
+    newTodos.splice(i, 1);
+    setTodos(newTodos);
+
   }
   return (
     <div className='App'>
       <h2>To DO List</h2>
-      <div className = "container" >
+      <div className="container" >
         <ul>
-          {todos.map((e) => {
-            return <li >{e} </li>
+          {todos.map(({ txt, completed }, i) => {
+            return (
+              <div>
+                <li
+                  className={completed ? "done" : ""}
+                  key={i}
+                  onClick={() => handleItemDone(i)}
+                >
+                  {txt}
+                </li>
+                <span
+                className="del"
+                onClick={() => handleDeleteItem(i)}
+
+                >
+                  ❎
+                </span>
+              </div>
+            )
           })}
         </ul>
         <input ref={inputRef} placeholder="Enter item" />
